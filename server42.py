13ea97b9f1c2ebd7c98260e1b36006a92a089273
zulhaziq86 <zulhaziq1216@gmail.com>
2020-12-13 20:10:55 +0800

    Lab 5

---
+++ server42.py
@@ -0,0 +1,24 @@
+import socket
+
+s = socket.socket()
+port = 6000
+s.bind(('', port))
+s.listen(5)
+
+print ("Server listening....")
+
+while True:
+    conn, addr = s.accept()
+    print ("Got connection from", addr)
+
+    filename='test.txt'
+    file = open(filename, 'wb')
+    print('receiving data...')
+    data = conn.recv(1024)
+    file.write(data
+
+    file.close()
+    print('Successfully get the file')
+    conn.close()
+    s.close()
+
